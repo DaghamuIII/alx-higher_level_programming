b0308107d4442f0a0ce1cb9519533770f7038aec
Amos Nyirenda <85877874+DaghamuIII@users.noreply.github.com>
2025-04-23 21:41:27 +0200

    Update 2-args.py II

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,24 +1,13 @@
 #!/usr/bin/python3
-import sys
-
-if __name__ == '__main__':
-    """Prints the argument list passed to the program
-
-    The program takes all the arguments starting from the second
-    and prints the number of arguments and their value
-
-    """
-    av = sys.argv
-    l_av = len(av) - 1
-
-    if l_av > 1:
-        print(l_av, 'arguments:')
-        for i in range(1, l_av + 1):
-            print('{:d}: {}'.format(i, av[i]))
-    elif l_av == 1:
-        print(l_av, 'argument:')
-        for i in range(1, l_av + 1):
-            print('{:d}: {}'.format(i, av[i]))
-    elif l_av == 0:
-        print(l_av, 'arguments.')
-
+if __name__ == "__main__":
+    import sys
+    num = len(sys.argv)
+    count = num - 1
+    if num == 1:
+        print("{} arguments.".format(count))
+    elif num == 2:
+        print("{} argument:".format(count))
+    else:
+        print("{} arguments:".format(count))
+    for i in range(1, num):
+        print("{}: {}".format(i, sys.argv[i]))
