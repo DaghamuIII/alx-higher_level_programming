674e7bf5b4e1e9495193629ec976351d558e823f
Amos Nyirenda <85877874+DaghamuIII@users.noreply.github.com>
2025-05-03 01:34:47 +0200

    Update 13-is_palindrome.c one more tme improving efficiency

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,61 +1,10 @@
 #include "lists.h"
 
-listint_t *reverse_list(listint_t *head);
-
 /**
- * is_palindrome - Checks if a singly linked list is a palindrome.
- * @head: Double pointer to the head of the list.
- * Return: 1 if palindrome, 0 if not.
- */
-int is_palindrome(listint_t **head)
-{
-    listint_t *slow = *head, *fast = *head;
-    listint_t *second_half, *prev = NULL;
-
-    if (*head == NULL || (*head)->next == NULL)
-        return (1);
-
-    // Find the middle using slow and fast pointers
-    while (fast && fast->next)
-    {
-        fast = fast->next->next;
-        prev = slow;
-        slow = slow->next;
-    }
-
-    // If list has odd number of elements, skip middle
-    if (fast)
-        slow = slow->next;
-
-    // Reverse the second half
-    second_half = reverse_list(slow);
-
-    // Compare both halves
-    listint_t *first_half = *head;
-    listint_t *check = second_half;
-    int palindrome = 1;
-
-    while (check)
-    {
-        if (first_half->n != check->n)
-        {
-            palindrome = 0;
-            break;
-        }
-        first_half = first_half->next;
-        check = check->next;
-    }
-
-    // Restore second half (optional but cleaner)
-    reverse_list(second_half);
-
-    return palindrome;
-}
-
-/**
- * reverse_list - Reverses a singly linked list.
- * @head: Pointer to the head node.
- * Return: New head of the reversed list.
+ * reverse_list - Reverses a linked list
+ * @head: Pointer to the first node of the list
+ *
+ * Return: New head of the reversed list
  */
 listint_t *reverse_list(listint_t *head)
 {
@@ -69,5 +18,54 @@ listint_t *reverse_list(listint_t *head)
         head = next;
     }
 
-    return prev;
+    return (prev);
+}
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: double pointer to the head of the list
+ *
+ * Return: 1 if it's a palindrome, 0 otherwise
+ */
+int is_palindrome(listint_t **head)
+{
+    listint_t *slow = *head, *fast = *head;
+    listint_t *second_half, *first_half, *reversed;
+    int result = 1;
+
+    if (!head || !*head || !(*head)->next)
+        return (1);
+
+    // Find the middle of the list
+    while (fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    // If the list has odd number of elements, skip the middle one
+    if (fast)
+        slow = slow->next;
+
+    // Reverse the second half
+    reversed = reverse_list(slow);
+    second_half = reversed;
+    first_half = *head;
+
+    // Compare the two halves
+    while (second_half)
+    {
+        if (first_half->n != second_half->n)
+        {
+            result = 0;
+            break;
+        }
+        first_half = first_half->next;
+        second_half = second_half->next;
+    }
+
+    // Restore the original list (optional but good practice)
+    reverse_list(reversed);
+
+    return (result);
 }
