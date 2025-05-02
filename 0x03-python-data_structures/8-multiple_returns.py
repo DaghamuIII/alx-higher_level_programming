4207f0ccab7171bc3b9bc9d4998aed41c3eaa613
Amos Nyirenda <85877874+DaghamuIII@users.noreply.github.com>
2025-05-02 23:56:30 +0200

    Update 8-multiple_returns.py2

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    """Returns a tuple: (length of sentence, first character or None)."""
     if sentence == "":
         return (0, None)
-    return (len(sentence), sentence[0]
+    return len(sentence), sentence[0]
