b0f0dfdcb0462ff06d31e4d36ba1ebb7a7aaafb4
Amos Nyirenda <85877874+DaghamuIII@users.noreply.github.com>
2025-05-03 00:43:58 +0200

    Update list.h

--- 0x03-python-data_structures/list.h
+++ 0x03-python-data_structures/list.h
@@ -1,25 +1,62 @@
-#ifndef LISTS_H
-#define LISTS_H
+#include "lists.h"
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for Holberton project
+ * reverse_list - Reverses a linked list
+ * @head: Pointer to the head pointer
+ * Return: New head of the reversed list
  */
-typedef struct listint_s
+listint_t *reverse_list(listint_t *head)
 {
-    int n;
-    struct listint_s *next;
-} listint_t;
+    listint_t *prev = NULL, *next = NULL;
 
-size_t print_listint(const listint_t *h);
-listint_t *add_nodeint_end(listint_t **head, const int n);
-void free_listint(listint_t *head);
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
-int is_palindrome(listint_t **head);
-size_t listint_len(const listint_t *h);
+    while (head)
+    {
+        next = head->next;
+        head->next = prev;
+        prev = head;
+        head = next;
+    }
 
-#endif /* LISTS_H */
+    return (prev);
+}
+
+/**
+ * is_palindrome - Checks if a singly linked list is a palindrome
+ * @head: Double pointer to head of list
+ * Return: 1 if palindrome, 0 otherwise
+ */
+int is_palindrome(listint_t **head)
+{
+    listint_t *slow, *fast, *rev, *temp;
+
+    if (head == NULL || *head == NULL)
+        return (1);
+
+    slow = fast = *head;
+
+    /* Find midpoint using slow and fast pointers */
+    while (fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    /* Reverse second half */
+    rev = reverse_list(slow);
+    temp = rev;
+
+    /* Compare first and second halves */
+    while (temp)
+    {
+        if ((*head)->n != temp->n)
+        {
+            reverse_list(rev);  /* Optional: restore original list */
+            return (0);
+        }
+        *head = (*head)->next;
+        temp = temp->next;
+    }
+
+    reverse_list(rev);  /* Optional: restore list */
+    return (1);
+}
