bc3248a8a1e8a42986bfe825f5cc892c3e6299da
Amos Nyirenda <85877874+DaghamuIII@users.noreply.github.com>
2025-05-03 01:30:12 +0200

    Update 13-is_palindrome.c improving eficieny by removing extra malloc allocation

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,61 +1,73 @@
-#include <stdlib.h>
 #include "lists.h"
 
+listint_t *reverse_list(listint_t *head);
+
 /**
- * is_palindrome - checks if a singly linked list is a palindrome
- * @head: double pointer to the head of the list
- *
- * Return: 1 if it's a palindrome, 0 otherwise
+ * is_palindrome - Checks if a singly linked list is a palindrome.
+ * @head: Double pointer to the head of the list.
+ * Return: 1 if palindrome, 0 if not.
  */
 int is_palindrome(listint_t **head)
 {
-    int *values;
-    int i, len;
-    listint_t *current;
+    listint_t *slow = *head, *fast = *head;
+    listint_t *second_half, *prev = NULL;
 
-    if (head == NULL || *head == NULL)
+    if (*head == NULL || (*head)->next == NULL)
         return (1);
 
-    len = listint_len(*head);
-    values = malloc(sizeof(int) * len);
-    if (values == NULL)
-        return (0);
-
-    current = *head;
-    for (i = 0; i < len; i++)
+    // Find the middle using slow and fast pointers
+    while (fast && fast->next)
     {
-        values[i] = current->n;
-        current = current->next;
+        fast = fast->next->next;
+        prev = slow;
+        slow = slow->next;
     }
 
-    for (i = 0; i < len / 2; i++)
+    // If list has odd number of elements, skip middle
+    if (fast)
+        slow = slow->next;
+
+    // Reverse the second half
+    second_half = reverse_list(slow);
+
+    // Compare both halves
+    listint_t *first_half = *head;
+    listint_t *check = second_half;
+    int palindrome = 1;
+
+    while (check)
     {
-        if (values[i] != values[len - 1 - i])
+        if (first_half->n != check->n)
         {
-            free(values);
-            return (0);
+            palindrome = 0;
+            break;
         }
+        first_half = first_half->next;
+        check = check->next;
     }
 
-    free(values);
-    return (1);
+    // Restore second half (optional but cleaner)
+    reverse_list(second_half);
+
+    return palindrome;
 }
 
 /**
- * listint_len - counts the number of elements in a linked list
- * @h: head of the list
- *
- * Return: number of elements
+ * reverse_list - Reverses a singly linked list.
+ * @head: Pointer to the head node.
+ * Return: New head of the reversed list.
  */
-size_t listint_len(const listint_t *h)
+listint_t *reverse_list(listint_t *head)
 {
-    int length = 0;
+    listint_t *prev = NULL, *next = NULL;
 
-    while (h != NULL)
+    while (head)
     {
-        length++;
-        h = h->next;
+        next = head->next;
+        head->next = prev;
+        prev = head;
+        head = next;
     }
 
-    return (length);
+    return prev;
 }
